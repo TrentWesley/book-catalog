a616502c5cd2a372d2ed1d4f84f96a7708a32a3c
Trent Wesley <tswesley1@buffs.wtamu.edu>
2026-09-12 16:00:13 -0500

    Add catalog models and migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    page_count = models.IntegerField()
+    publisher = models.ForeignKey(
+        Publisher,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    body = models.TextField()
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.body
