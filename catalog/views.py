818a281592144e0108dcb5bc790e1cab629fccaf
Trent Wesley <tswesley1@buffs.wtamu.edu>
2026-09-12 19:40:41 -0500

    Add catalog pages and navigation

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book, Publisher, Review
 
-# Create your views here.
+
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+
+
+class PublisherListView(ListView):
+    model = Publisher
+    template_name = "publisher_list.html"
+
+
+class ReviewListView(ListView):
+    model = Review
+    template_name = "review_list.html"
